e756ca19c8e19e17963013d4a39bf1c3a6aba1c0
imtananraja <imtanan.raja@hotmail.com>
2019-10-30 16:35:19 +0000

    scan for a banner in open ports

--- scanning/advancescanner.py
+++ scanning/advancescanner.py
@@ -6,6 +6,34 @@ from socket import *
 import optparse
 from threading import *
 
+def connScan(tgtHost, tgtPort):
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.connect((tgtHost, tgtPort))
+        print "%d/tcp Open" % tgtPort
+    except:
+        print "%d/tcp Closed" % tgtPort
+    finally:
+        sock.close()
+
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = gethostbyname(tgtHost)
+    except:
+        print 'Unknown host %s' % tgtHost
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print "Scan results for: " + tgtName[0]
+    except:
+        print 'Scan results for: ' + tgtIP
+    setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        # create a thread to check for ports available in the host
+        # target= the function
+        # args= the arguments to be passed into the function
+        t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
+
 def main():
     # this will help the user understand how to use the program
     parser = optparse.OptionParser('Usage of program: ' + '-H <target host> -p <target ports>')
@@ -21,7 +49,13 @@ def main():
     if (tgtHost == None) | (tgtPorts[0] == None):
         print parser.usage
         exit(0)
-    #portScan(tgtHost, tgtPorts)
+    portScan(tgtHost, tgtPorts)
 
-main()
+if __name__ == '__main__':
+	main()
 
+# To call this program
+# python advancescanner.py -H 192.168.1.10 -p 8,10
+# python advancescanner.py -H google.com -p 443,8080,92
+# python advancescanner.py -H 18.90.23.54 -p 330
+# python advancescanner.py -H bbc.co.uk -p 443
---
+++ scanning/returnBanner.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import socket
+
+
+def returnbanner(ip, port):
+    try:
+        # timeout if no response in 2 seconds
+        socket.setdefaulttimeout(2)
+        # create a socket object
+        s = socket.socket()
+        # connect to the ip and port
+        s.connect((ip, port))
+        # what we receive from the host when we scan
+        # and the number of bytes to receive
+        banner = s.recv(1024)
+        return banner
+    except:
+        return
+
+
+def main():
+    ip = raw_input("Enter target IP: ")
+    for port in range(1, 100):
+        banner = returnbanner(ip, port)
+        if banner:
+            print "[+] " + ip + "on Port " + str(port) + " : " + banner
+
+
+if __name__ == '__main__':
+    main()
+
+# for metasploitable machine
+# ip = 10.64.240.204
+# port = 22
+# response is [+] 10.64.240.204: SSH-2.0-OpenSSH_4.7p1 Debian-8ubuntu1
+# you can use the 'SSH-2.0-OpenSSH_4.7p1 Debian-8ubuntu1'
+# and search for exploits/vunerbilities
