21289cfd5b05540078c980429ee7426eff7a412e
imtananraja <imtanan.raja@hotmail.com>
2019-11-07 18:02:32 +0000

    sorry for the swears in the passfile, I copied it from the internet. Also starting bruteforcing a crypt password

---
+++ cracking_passwords/cryptbrute.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# to syntax check run
+# python -m py_compile <filename>
+
+import crypt
+
+def main():
+    print('ok')
+
+
+if __name__ == '__main__':
+    main()
+
+
--- cracking_passwords/md5brute.py
+++ cracking_passwords/md5brute.py
@@ -12,20 +12,36 @@ def tryOpen(passwordFile):
         quit()
 
 def getMd5Pass(passFile, passwordHash):
+    # start reading from the beginning of the file
+    passFile.seek(0)
+    print(colored("Now running the decrypting of md5 hash: ", 'white'))
     for password in passFile:
         print(colored("Trying: " + password.strip("\n"), 'blue'))
         # Have to encode word because it is python3
         encodePass = password.encode('utf-8')
-        # Now we are going to hash the password with md5
+        # Now we are going to hash the password with md5 and strip and extras on the password
         # 1) Makes the object of md5
         # 2) then hexdigest() turns it into a md5 hash
         md5digest = hashlib.md5(encodePass.strip()).hexdigest()
 
         if md5digest == passwordHash:
             print(colored("Password found: " + password, 'green'))
-            exit(0)
-
+            return
     print(colored("Password is not in list!", 'red'))
+
+def getSha256(passFile, passwordHash):
+    passFile.seek(0)
+    print(colored("Now running the decrypting of sha256 hash: ", 'white'))
+    for password in passFile:
+        print(colored("Trying: " + password.strip("\n"), 'blue'))
+        encodePass = password.encode('utf-8')
+        sha256digest = hashlib.sha256(encodePass.strip()).hexdigest()
+
+        if passwordHash == sha256digest:
+            print(colored("The sha256 password hash is: " + password, 'green'))
+            return
+    print(colored("Password not found in list", 'red'))
+
 
 # TODO: Create sha1, sha512, sha256 password decrypter and as many others in the hashlib library
 
@@ -34,6 +50,7 @@ def main():
     passwordFile = input("Please specify the password file: ")
     passFile = tryOpen(passwordFile)
     # run the function to get md5 hashed password
+    getSha256(passFile, passwordHash)
     getMd5Pass(passFile, passwordHash)
 
 
